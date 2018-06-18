58382ca89dfd578cd337cf367178bc449c152ab1
Oscar <oskarromero3@gmail.com>
2018-06-18 17:15:52 +0100

    hookup admin|member profile community spec

--- spec/models/admin_profile_community_spec.rb
+++ spec/models/admin_profile_community_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe AdminProfileCommunity, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:community) }
+  it { should belong_to(:admin_profile) }
 end

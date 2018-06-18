cd5c7f08e20ce46872ff768e87ee244bc2838a23
Oscar <oskarromero3@gmail.com>
2018-06-18 12:41:44 +0100

    add basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user
 
   helper_method :current_user
 
@@ -7,5 +8,11 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  private
+
+  def authenticate_user
+    redirect_to root_path unless current_user.present?
   end
 end

b8af2fc9fd5d3cb5873d47ced5102efcfbc631d2
Oscar <oskarromero3@gmail.com>
2018-06-18 17:13:27 +0100

    hookup admin profile spec

--- spec/models/admin_profile_spec.rb
+++ spec/models/admin_profile_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe AdminProfile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:user) }
+  it { should have_many(:admin_profile_communities) }
+  it { should have_many(:communities).through(:admin_profile_communities) }
+
+  describe 'aliases' do
+    it 'aliases #admin_communities as communities' do
+      expect(subject.admin_communities).to eq subject.communities
+    end
+  end
 end

a20a2ebda4a664451f581518dcef19874445eab9
Oscar <oskarromero3@gmail.com>
2018-06-18 17:11:20 +0100

    hookup member profile spec

--- app/models/member_profile.rb
+++ app/models/member_profile.rb
@@ -2,7 +2,7 @@ class MemberProfile < ApplicationRecord
   belongs_to :user
 
   has_many :member_profile_communities, dependent: :destroy
-  has_many :communities, through: :admin_profile_communities
+  has_many :communities, through: :member_profile_communities
 
   alias member_communities communities
 end
--- spec/models/member_profile_spec.rb
+++ spec/models/member_profile_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe MemberProfile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:user) }
+  it { should have_many(:member_profile_communities) }
+  it { should have_many(:communities).through(:member_profile_communities) }
+
+  describe 'aliases' do
+    it 'aliases #member_communities as communities' do
+      expect(subject.member_communities).to eq subject.communities
+    end
+  end
 end

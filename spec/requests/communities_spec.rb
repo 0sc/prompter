ee2876ed76a0023273c60d9419690e783f296b86
Oscar <oskarromero3@gmail.com>
2018-06-23 23:48:14 +0100

    remove unnecessary spec for now

--- spec/requests/communities_spec.rb
+++ spec/requests/communities_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe "Communities", type: :request do
-  describe "GET /communities" do
-    it "works! (now write some real specs)" do
+RSpec.describe 'Communities', type: :request do
+  describe 'GET /communities' do
+    xit 'works! (now write some real specs)' do
       get communities_path
       expect(response).to have_http_status(200)
     end

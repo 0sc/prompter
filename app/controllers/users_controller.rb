faa6dbbf1e9f553cd09d25ec81326afe55f68eb2
Oscar <oskarromero3@gmail.com>
2018-07-01 15:24:36 +0100

    implement account linking

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,66 @@
 class UsersController < ApplicationController
   skip_before_action :authenticate_user
+  before_action :ensure_valid_user, only: :account_link
+  before_action :set_session_variables, only: :account_link
+  after_action :clear_session, if: -> { account_linking? }, only: :create
 
   def new; end
 
   def create
-    user = User.find_or_initialize_by(fbid: auth_hash[:uid])
+    # TODO: logic to combine accounts probably copy messenger account over
+    user = current_user || User.find_or_initialize_by(fbid: auth_hash[:uid])
     user.update_from_auth_hash(auth_hash)
 
     if user.save
       session[:user_id] = user.id
-      redirect_to communities_path, notice: "Welcome #{user.name}"
+      redirect_to success_redirect_uri, notice: "Welcome #{user.name}"
     else
-      redirect_to root_path, notice: 'Error occured setting up your account!!'
+      msg = 'Error occured setting up your account!'
+      redirect_to failed_redirect_uri, notice: msg
     end
+  end
+
+  def account_link
+    redirect_to '/auth/facebook'
   end
 
   protected
 
   def auth_hash
     request.env['omniauth.auth']
+  end
+
+  private
+
+  def ensure_valid_user
+    @current_user = User.find_by(psid: params[:psid])
+    redirect_to root_path unless current_user.present?
+  end
+
+  def set_session_variables
+    session[:alt] = params['account_linking_token']
+    session[:rdr] = params['redirect_uri']
+    session[:user_id] = current_user.id
+  end
+
+  def success_redirect_uri
+    if account_linking?
+      "#{session[:rdr]}&authorization_code=account-linked-successfully"
+    else
+      communities_path
+    end
+  end
+
+  def failed_redirect_uri
+    account_linking? ? session[:rdr] : root_path
+  end
+
+  def account_linking?
+    session[:rdr].present?
+  end
+
+  def clear_session
+    # session.clear
+    %i[alt rdr].each { |key| session.delete(key) }
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -61,21 +61,95 @@ RSpec.describe UsersController, type: :controller do
         end
       end
 
-      it 'redirects to the communities_path afterwards' do
-        post :create
-        expect(response).to redirect_to communities_path
-        expect(flash[:notice]).to eq "Welcome #{User.first.name}"
+      describe 'redirect' do
+        context 'account linking' do
+          it 'redirects to the session rdr afterwards' do
+            uri = 'https://fb.com/bot/way?code=green'
+            payload = '&authorization_code=account-linked-successfully'
+            post :create, params: {}, session: { rdr: uri }
+            expect(response).to redirect_to "#{uri}#{payload}"
+            expect(flash[:notice]).to eq "Welcome #{User.first.name}"
+          end
+        end
+
+        context 'not account linking' do
+          it 'redirects to the communities_path afterwards' do
+            post :create
+            expect(response).to redirect_to communities_path
+            expect(flash[:notice]).to eq "Welcome #{User.first.name}"
+          end
+        end
       end
     end
 
     describe 'unsuccessful create' do
-      it 'redirects to the root path with notice' do
-        request.env['omniauth.auth'] = { info: {}, credentials: {} }
+      let(:notice) { 'Error occured setting up your account!' }
+      before { request.env['omniauth.auth'] = { info: {}, credentials: {} } }
 
-        post :create
+      describe 'redirect' do
+        context 'account linking' do
+          it 'redirects to the session rdr afterwards' do
+            uri = 'https://fb.com/bot/way?code=green'
+            post :create, params: {}, session: { rdr: uri }
+            expect(response).to redirect_to uri
+            expect(flash[:notice]).to eq notice
+          end
+        end
 
-        expect(response).to redirect_to '/'
-        expect(flash[:notice]).to eq 'Error occured setting up your account!!'
+        context 'not account linking' do
+          it 'redirects to the root path with notice' do
+            post :create
+
+            expect(response).to redirect_to '/'
+            expect(flash[:notice]).to eq notice
+          end
+        end
+      end
+    end
+
+    describe 'clear_session' do
+      context 'account_linking?' do
+        it 'clears the account linking session vars' do
+          post :create, params: {}, session: { rdr: 'something', alt: 'herre' }
+          expect(session['alt']).to eql nil
+          expect(session['rdr']).to eql nil
+        end
+      end
+
+      context 'not account linking' do
+        it 'does not clear the session vars' do
+          post :create, params: {}, session: { alt: 'herre' }
+          expect(session['alt']).to eql 'herre'
+        end
+      end
+    end
+  end
+
+  describe 'GET #account_link' do
+    context 'invalid user' do
+      it 'redirects to root_path' do
+        get :account_link, params: { psid: 404 }
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context 'valid user' do
+      subject { create(:user) }
+
+      it 'sets account linking session variables' do
+        get :account_link,
+            params: {
+              psid: subject.psid,
+              redirect_uri: 'https://somewhere.deep/on/fb',
+              account_linking_token: 'fb-super-secret-token'
+            }
+        expect(session[:rdr]).to eq 'https://somewhere.deep/on/fb'
+        expect(session['alt']).to eq 'fb-super-secret-token'
+      end
+
+      it 'redirects to fb auth route' do
+        get :account_link, params: { psid: subject.psid }
+        expect(response).to redirect_to '/auth/facebook'
       end
     end
   end

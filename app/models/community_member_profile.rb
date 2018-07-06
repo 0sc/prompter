6cfdf4f279129f772e5ecdb2c23568b5cf15787a
Oscar <oskarromero3@gmail.com>
2018-07-06 18:33:22 +0100

    add community_member_profile has many association to community_member_profile_feed_categories

--- app/models/community_member_profile.rb
+++ app/models/community_member_profile.rb
@@ -1,6 +1,8 @@
 class CommunityMemberProfile < ApplicationRecord
   belongs_to :member_profile
   belongs_to :community
+  has_many :community_member_profile_feed_categories, dependent: :destroy
+  has_many :feed_categories, through: :community_member_profile_feed_categories
 
   validates_presence_of :community_id, :member_profile_id
   validates_uniqueness_of :community_id, scope: :member_profile_id
--- spec/models/community_member_profile_spec.rb
+++ spec/models/community_member_profile_spec.rb
@@ -5,6 +5,14 @@ RSpec.describe CommunityMemberProfile, type: :model do
 
   it { should belong_to(:community) }
   it { should belong_to(:member_profile) }
+  it do
+    should have_many(:community_member_profile_feed_categories)
+      .dependent(:destroy)
+  end
+  it do
+    should have_many(:feed_categories)
+      .through(:community_member_profile_feed_categories)
+  end
 
   it { should validate_presence_of(:community_id) }
   it { should validate_presence_of(:member_profile_id) }

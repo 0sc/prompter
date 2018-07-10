61f4a40b86c988108521bad51987442d00be1018
Oscar <oskarromero3@gmail.com>
2018-07-10 16:48:41 +0100

    add token_ttl worker

---
+++ app/workers/token_ttl_worker.rb
@@ -0,0 +1,47 @@
+class TokenTtlWorker
+  include Sidekiq::Worker
+
+  def perform
+    # TODO: consider sending email notifications where psid is missing
+    # TODO: consider unsubscribing communities with expired tokens
+    each_user_with_just_expired_token do |user|
+      num = user.admin_communities.count
+      Notifier.send_access_token_expired_notice(
+        psid: user.psid, num_admin_comms: num
+      )
+    end
+
+    each_user_with_expiring_token do |user|
+      num = user.admin_communities.count
+      Notifier.send_access_token_expiring_notice(
+        psid: user.psid, num_admin_comms: num
+      )
+    end
+  end
+
+  private
+
+  def each_user_with_just_expired_token(&blk)
+    raise 'I need a block' unless block_given?
+    range = 1.hour.ago.to_i
+    now = Time.current.to_i
+
+    User
+      .where(expires_at: range..now)
+      .where.not(psid: nil)
+      .find_each(&blk)
+  end
+
+  def each_user_with_expiring_token(&blk)
+    raise 'I need a block' unless block_given?
+    range = 2.days.from_now.to_i
+    now = Time.current.to_i
+
+    User
+      .where(expires_at: now..range)
+      .where.not(psid: nil)
+      .find_each(&blk)
+  end
+end
+
+### runs every day
---
+++ spec/workers/token_ttl_worker_spec.rb
@@ -0,0 +1,66 @@
+require 'rails_helper'
+RSpec.describe TokenTtlWorker, type: :worker do
+  let(:user_one) { create(:user, expires_at: Time.current.to_i) }
+  let(:user_two) { create(:user, expires_at: 1.year.ago.to_i) }
+  let(:user_three) { create(:user, expires_at: 1.year.from_now.to_i) }
+
+  before do
+    create(:community_admin_profile, admin_profile: user_one.admin_profile)
+    create(:community_admin_profile, admin_profile: user_two.admin_profile)
+    create(:community_admin_profile, admin_profile: user_three.admin_profile)
+  end
+
+  describe 'admin profiles with tokens expiring withing 2 days' do
+    before { user_one.update!(expires_at: 110.minutes.from_now.to_i) }
+
+    it 'sends send_access_token_expiring_notice if psid is present' do
+      TokenTtlWorker.perform_async
+      expect(TokenTtlWorker.jobs.size).to eq 1
+      expect(TokenTtlWorker.jobs.first['args']).to be_empty
+
+      expect(Notifier).to receive(:send_access_token_expiring_notice)
+        .with(psid: user_one.psid, num_admin_comms: 1)
+
+      TokenTtlWorker.drain
+    end
+
+    it 'does not send notice if psid is missing' do
+      user_one.update(psid: nil)
+      expect(Notifier).not_to receive(:send_access_token_expiring_notice)
+      TokenTtlWorker.drain
+    end
+
+    it 'does not send notice if there are no assoc admin_communities' do
+      user_one.admin_communities.map(&:destroy!)
+      expect(Notifier).not_to receive(:send_access_token_expiring_notice)
+      TokenTtlWorker.drain
+    end
+  end
+
+  describe 'admin profiles with tokens that expired within 1 hour' do
+    before { user_one.update(expires_at: 40.minutes.ago.to_i) }
+
+    it 'sends send_access_token_expired_notice if psid is present' do
+      TokenTtlWorker.perform_async
+      expect(TokenTtlWorker.jobs.size).to eq 1
+      expect(TokenTtlWorker.jobs.first['args']).to be_empty
+
+      expect(Notifier).to receive(:send_access_token_expired_notice)
+        .with(psid: user_one.psid, num_admin_comms: 1)
+
+      TokenTtlWorker.drain
+    end
+
+    it 'does not send notice if psid is missing' do
+      user_one.update(psid: nil)
+      expect(Notifier).not_to receive(:send_access_token_expired_notice)
+      TokenTtlWorker.drain
+    end
+
+    it 'does not send notice if there are no assoc admin_communities' do
+      user_one.admin_communities.map(&:destroy!)
+      expect(Notifier).not_to receive(:send_access_token_expired_notice)
+      TokenTtlWorker.drain
+    end
+  end
+end

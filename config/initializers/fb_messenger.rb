ff0e8bf45e9e01cd79ebc7b68cb20b090aa4c77f
Oscar <oskarromero3@gmail.com>
2018-07-12 09:27:50 +0100

    add messenger setup

--- config/initializers/fb_messenger.rb
+++ config/initializers/fb_messenger.rb
@@ -18,6 +18,58 @@ class CredentialProvider < Facebook::Messenger::Configuration::Providers::Base
   end
 end
 
-Facebook::Messenger.configure do |config|
-  config.provider = CredentialProvider.new
+class MessengerProfileSetup
+  def bulk
+    set_get_started
+      .merge(set_whitelist_domains)
+      .merge(set_greeting_text)
+      .merge(set_persistent_menu)
+  end
+
+  def set_get_started
+    { get_started: { payload: 'GET_STARTED_PAYLOAD' } }
+  end
+
+  def set_whitelist_domains
+    { whitelisted_domains: [ENV.fetch('HOST_URL')] }
+  end
+
+  def set_greeting_text
+    {
+      greeting: [
+        { locale: 'default', text: 'Welcome {{user_full_name}}, to your new bot overlord!' },
+        { locale: 'fr_FR', text: 'Bienvenue {{user_full_name}}, dans le bot du Wagon !' }
+      ]
+    }
+  end
+
+  def set_persistent_menu
+    actions = %w[find_communities subscribe_communities manage_communities]
+    cta = actions.map do |action|
+      {
+        title: action.tr('_', ' ').titleize,
+        type: 'postback',
+        payload: "ChatService::#{action.upcase}".constantize
+      }
+    end
+
+    {
+      persistent_menu: [{
+        locale: 'default',
+        composer_input_disabled: true,
+        call_to_actions: cta
+      }]
+    }
+  end
+end
+
+unless Rails.env.test?
+  access_token = Rails.application.credentials.page_access_token
+
+  Facebook::Messenger.configure do |config|
+    config.provider = CredentialProvider.new
+  end
+
+  payload = MessengerProfileSetup.new.bulk
+  Facebook::Messenger::Profile.set(payload, access_token: access_token)
 end

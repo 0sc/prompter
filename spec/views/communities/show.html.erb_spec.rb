299add51127601762a126904d9c385b555699368
Oscar <oskarromero3@gmail.com>
2018-07-19 04:25:38 +0100

    bug-fix: check for presence of cover image before attempting to display image

--- spec/views/communities/show.html.erb_spec.rb
+++ spec/views/communities/show.html.erb_spec.rb
@@ -16,48 +16,77 @@ RSpec.describe 'communities/show', type: :view do
 
     assign(:community, community)
     stub_const('ENV', 'BOT_URL' => 'm.me/meee')
-    render
   end
 
   it 'displays the nav bar' do
+    render
+
     expect(page).to have_selector('nav')
   end
 
-  it 'displays the community cover image' do
-    expect(page).to have_selector("img[src='#{community.cover}']")
+  describe 'cover image' do
+    it 'displays the community cover image if present' do
+      render
+
+      expect(page).to have_selector("img[src='#{community.cover}']")
+    end
+
+    it 'does not display any image if no cover image' do
+      community.update(cover: nil)
+      render
+
+      expect(page).not_to have_selector("img[src='#{community.cover}']")
+    end
+
   end
 
   it 'displays the community name' do
+    render
+
     expect(page).to have_content(community.name)
   end
 
   it 'displays the community type name' do
+    render
+
     expect(page).to have_content(community.community_type_name.titleize)
   end
 
   it 'displays the number of subscribed member profiles' do
+    render
+
     expect(page).to have_content(community.member_profiles.count)
   end
 
   it 'displays link with referral_code' do
+    render
+
     link = 'm.me/meee?ref=' + community.referral_code
     expect(page).to have_content(link)
   end
 
   it 'displays the qrcode' do
+    render
+
     expect(page).to have_selector("img[src='#{community.qrcode}']")
   end
 
   it 'displays link to edit the community details' do
+    render
+
     expect(page)
       .to have_link(text: t('cta.edit'), href: edit_community_path(community))
   end
 
   it 'displays link to go back to communities path' do
+    render
+
     expect(page).to have_link(text: t('cta.back'), href: communities_path)
   end
 
   it 'displays the message button' do
+    render
+
     expect(page).to have_selector('div.fb-messengermessageus')
   end
 

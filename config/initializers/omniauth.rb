56a439ee1e81bfc239b448ff32c72f56c95fb06b
Oscar <oskarromero3@gmail.com>
2018-07-27 01:13:30 +0100

    request only group_access_memeber_info permission

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider(:facebook, ENV.fetch('FACEBOOK_KEY'), ENV.fetch('FACEBOOK_SECRET'),
-           scope: 'email,publish_to_groups,groups_access_member_info')
+           scope: 'email,groups_access_member_info')
   # user_managed_groups
 end
